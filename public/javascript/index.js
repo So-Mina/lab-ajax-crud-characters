bee656c4b3df5869e9e102f8003d5259782cf191
Sophie Dtr <119942370+So-Mina@users.noreply.github.com>
2023-02-17 11:47:10 +0100

    Iteration 1 done with Axios Api

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,26 +1,53 @@
   /**
    * You might want to use this template to display each new characters
    * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
+   */  
+  
+  /*
+   * <template id="template">
+			<div class="character-info">
+				<div class="character-id">Id: <span></span></div>
+				<div class="name">Name: <span></span></div>
+				<div class="occupation">Occupation: <span></span></div>
+				<div class="cartoon">Cartoon: <span></span></div>
+				<div class="weapon">Weapon: <span></span></div>
+			</div>
+		</template>
    */
-  const characterTemplate = document.getElementById('template')
 
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+const characterTemplate = document.getElementById('template')
+
+/**
+ * Create an instance of axios
+ */
+const characterApi = axios.create({
+  baseURL: 'http://localhost:5005/api/ajax-crud-characters/characters'
+})
+
+const myUrl = 'http://localhost:5005/api/'
+
+
+
+  document.getElementById('fetch-all').addEventListener('click', fetchAll (event) {
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('fetch-one').addEventListener('click', fetchOne (event) {
 
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document.getElementById('delete-one').addEventListener('click', deleteOne (event) {
 
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+
+
+
+  document.getElementById('edit-character-form').addEventListener('submit', editOne (event) {
 
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', newOne (event) {
 
   });
 
--- routes/characters.route.js
+++ routes/characters.route.js
@@ -1,45 +1,96 @@
 const router = require('express').Router()
 const Character = require('../models/Character.model')
+const { isValidObjectId } = ('mongoose')
 /**
  * !All the routes here are prefixed with /api/characters
  */
 
 /**
  * ? This route should respond with all the characters
+ * CODE 200 : OK
  */
-router.get('/', (req, res, next) => {
-	/**Your code goes here */
+router.get('/', async (req, res, next) => {
+	try {
+		const allCharacters = await Character.find()
+		res.status(200).json(allCharacters)
+	} catch (error) {
+		next(error)
+	}
 })
 
 /**
  * ? This route should create one character and respond with
  * ? the created character
+ * code 201 : CREATED
  */
-router.post('/', (req, res, next) => {
-	/**Your code goes here */
+router.post('/', async (req, res, next) => {
+	try {
+		/* const {name, occupation, weapon, cartoon} = req.body
+		 	const createCharacter = await Character.create(req.body) */
+		/* const charData = {name, occupation, weapon, cartoon}
+			const createCharacter = await Character.create(charData) */
+		const characterToCreate = {...req.body}
+		const createCharacter = await Character.create(characterToCreate)
+		res.status(201).json(createCharacter)
+	} catch (error) {
+		next({ errorMessage: 'Invalid or missing field'})
+	}
 })
 
 /**
  * ? This route should respond with one character
  */
-router.get('/:id', (req, res, next) => {
-	/**Your code goes here */
+router.get('/:id', async (req, res, next) => {
+	/* const {id} = req.params
+		const idChar = req.params.id */
+	try {
+		const oneCharacter = await Character.findById(req.params.id)
+		res.json(oneCharacter)
+	} catch (error) {
+		next(error)
+	}
 })
 
 /**
  * ? This route should update a character and respond with
  * ? the updated character
  */
-router.patch('/:id', (req, res, next) => {
-	/**Your code goes here */
+router.patch('/:id', async (req, res, next) => {
+	try {
+		/* const {name, occupation, weapon, cartoon} = req.body
+			const characterToUpdate = {} 
+			if (name) {
+				characterToUpdate.name = name }
+				and so on... */
+		
+		if (!isValidObjectId(req.params.id)) {
+			return res.status(400).json({message: 'The id is not correct'})
+		}
+		const foundCharacter = await Character.findById(req.params.id)
+		if (!foundCharacter) {
+			return res.status(400).json({message: 'No character found for this id'})
+		}
+
+		const characterId = req.params.id
+		const characterToUpdate = { ...req.body}
+		const updatedCharacter = await Character.findByIdAndUpdate(characterId, characterToUpdate, {new: true})
+		res.status(202).json(updatedCharacter)
+	} catch (error) {
+		next(error)
+	}
 })
 
 /**
  * ? Should delete a character and respond with a success or
  * ? error message
  */
-router.delete('/:id', (req, res, next) => {
-	/**Your code goes here */
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const character = await Character.findByIdAndDelete(req.params.id)
+      res.json({message: `${character} has been successfully deleted`})
+    } catch (error) {
+		next(error)
+	}
 })
 
 module.exports = router
